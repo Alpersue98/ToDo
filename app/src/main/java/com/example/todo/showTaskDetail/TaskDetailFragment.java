e1e0aee9728524b5579b75a1027c271b00bf4d30
Alpersue98 <85168399+Alpersue98@users.noreply.github.com>
2022-06-16 12:19:26 +0200

    Textview sieht man && kp abstände

--- app/src/main/java/com/example/todo/showTaskDetail/TaskDetailFragment.java
+++ app/src/main/java/com/example/todo/showTaskDetail/TaskDetailFragment.java
@@ -59,29 +59,29 @@ public class TaskDetailFragment extends Fragment  implements View.OnClickListene
         currentTaskPos = taskPos;
 
         //Task Name
-        //textView = (TextView) getView().findViewById(R.id.nameView);
-        //textView.setText("Task");
+        textView = (TextView) getView().findViewById(R.id.nameView);
+        textView.setText("Task");
         editName = (EditText) getView().findViewById(R.id.editName);
         //Get and display task name
         editName.setText(currentTask.getShortName());
 
         //Description
-        //textView = (TextView) getView().findViewById(R.id.descView);
-        //textView.setText("Description");
+        textView = (TextView) getView().findViewById(R.id.descView);
+        textView.setText("Description");
         editDesc = (EditText) getView().findViewById(R.id.editDescription);
         //Get and display task description
         editDesc.setText(currentTask.getDescription());
 
         //CreationDate
-        //textView = (TextView) getView().findViewById(R.id.dateView);
-        //textView.setText("Creation Date");
+        textView = (TextView) getView().findViewById(R.id.dateView);
+        textView.setText("Creation Date");
         textView = (TextView) getView().findViewById(R.id.text_Date);
         //Get and display creation date
         textView.setText(DateFormat.getDateInstance().format(currentTask.getCreationDate()));
 
         //Done
-        //textView = (TextView) getView().findViewById(R.id.doneView);
-        //textView.setText("Done");
+        textView = (TextView) getView().findViewById(R.id.doneView);
+        textView.setText("Done");
         doneBox = (CheckBox) getView().findViewById(R.id.checkBox);
         //Set box as checked is task is saved as done
         doneBox.setChecked( currentTask.isDone());
